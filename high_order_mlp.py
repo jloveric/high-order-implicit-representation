7fbb25fda3d9f122ed855cec4db920cd37b86180
jloveric <john.loverich@gmail.com>
2020-12-29 06:40:48 -0800

    ability to add non-linearity

--- high_order_mlp.py
+++ high_order_mlp.py
@@ -19,7 +19,7 @@ class HighOrderMLP(nn.Module):
         scale: float = 2.0,
         rescale_output: bool = False,
         periodicity: float = None,
-        non_linearity: None
+        non_linearity=None
     ) -> None:
         super().__init__()
         layer_list = []
@@ -27,12 +27,14 @@ class HighOrderMLP(nn.Module):
                                            segments=in_segments, rescale_output=rescale_output, scale=scale, periodicity=periodicity)
         layer_list.append(input_layer)
         for i in range(hidden_layers):
-            if non_linearity is not None :
+            if non_linearity is not None:
                 layer_list.append(non_linearity())
             hidden_layer = high_order_fc_layers(layer_type=layer_type, n=n, in_features=hidden_width, out_features=hidden_width,
                                                 segments=hidden_segments, rescale_output=rescale_output, scale=scale, periodicity=periodicity)
             layer_list.append(hidden_layer)
 
+        if non_linearity is not None:
+            layer_list.append(non_linearity())
         output_layer = high_order_fc_layers(layer_type=layer_type, n=n, in_features=hidden_width, out_features=out_width,
                                             segments=out_segments, rescale_output=rescale_output, scale=scale, periodicity=periodicity)
         layer_list.append(output_layer)
