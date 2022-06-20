e1f805c8ab911576d1856e3e335cd9958a7b5a3c
John <john.loverich@gmail.com>
2022-06-20 07:18:37 -0700

    Fixing tests

---
+++ tests/test_single_image_dataset.py
@@ -0,0 +1,13 @@
+import pytest
+from high_order_implicit_representation.single_image_dataset import (
+    image_to_dataset,
+    ImageNeighborhoodReader,
+)
+
+
+def test_image_to_dataset():
+    assert True is True
+
+
+def test_image_neighborhood_reader():
+    assert True is True
