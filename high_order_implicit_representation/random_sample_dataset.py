Fixing some normalization issues

high_order_implicit_representation/random_sample_dataset.py
@@ -42,6 +42,7 @@ class RandomImageSampleDataset(Dataset):
                 transforms.RandomHorizontalFlip(),
                 transforms.CenterCrop(image_size),
                 transforms.ToTensor(),
+                transforms.Normalize(mean=0.5, std=0.5)
             ]
         )
 
@@ -138,6 +139,7 @@ class RadialRandomImageSampleDataset(Dataset):
                 transforms.RandomHorizontalFlip(),
                 transforms.CenterCrop(image_size),
                 transforms.ToTensor(),
+                transforms.Normalize(mean=0.5, std=0.5)
             ]
         )
 
@@ -237,8 +239,10 @@ def random_symmetric_sample(
     r = torch.rand(num_samples, interp_size, device=device)
     theta = torch.rand(num_samples, interp_size, device=device) * 2 * math.pi
 
-    x = (r * torch.cos(theta) * image_size).int()
-    y = (r * torch.sin(theta) * image_size).int()
+    # a pixel at the center should sample the edge but
+    # not further.
+    x = (0.5*r * torch.cos(theta) * image_size).int()
+    y = (0.5*r * torch.sin(theta) * image_size).int()
 
     xy = torch.stack([x, y]).permute(2, 1, 0)
     xy = xy + samples

high_order_implicit_representation/utils.py
@@ -51,7 +51,7 @@ def generate_sample_radial(
     if image is None:
         image = torch.rand([3, image_size, image_size], device=device) * 2 - 1
     else:
-        image = (image / 256) * 2 - 1
+        image = (image / 255) * 2 - 1
 
     stripe_list = positions_from_mesh(
         width=image_size,
@@ -129,7 +129,7 @@ def generate_sample(
     if image is None:
         image = torch.rand([3, image_size, image_size], device=device) * 2 - 1
     else:
-        image = (image / 256) * 2 - 1
+        image = (image / 255) * 2 - 1
 
     stripe_list = positions_from_mesh(
         width=image_size,

tests/test_random_sample_dataset.py
@@ -26,7 +26,8 @@ def test_radial_random_image_sample_dataset_specific():
     )
 
     features, targets = this_iter.next()
-
+    print('features', features)
+    print('targets', targets)
     assert features.shape[0] == targets.shape[0] == 2048
     assert features.shape[1] == num_feature_pixels
     assert targets.shape[1] == num_target_pixels
@@ -42,7 +43,7 @@ def test_random_symmetric_sample():
     result = random_symmetric_sample(
         image_size=image_size, interp_size=interp_size, samples=samples
     )
-
+    print('result', result)
     assert torch.all(result < image_size).tolist() is True
     assert torch.all(result >= 0).tolist() is True
 
@@ -124,4 +125,5 @@ def test_random_image_sample_datamodule():
     assert targets.shape[1] == 1
     assert features.shape[2] == 5
     assert targets.shape[2] == 3
+
 