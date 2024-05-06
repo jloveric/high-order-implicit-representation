85bde1aac220da2a11dc1ac447131242271f5b83
jloveric <john.loverich@gmail.com>
2024-05-05 17:20:48 -0700

    Just getting parquet loader working

--- examples/text_to_image.py
+++ examples/text_to_image.py
@@ -1,1 +1,7 @@
 # This is my first effort at making an interpolated dictionary
+# This will be an image generator based on parquet data https://huggingface.co/datasets/yuvalkirstain/pickapic_v2/tree/main/data
+
+import pandas as pd
+
+ans = pd.read_parquet("train-00000-of-00645-b66ac786bf6fb553.parquet")
+print('ans', ans)
